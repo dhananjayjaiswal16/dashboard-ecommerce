1b174c507d74aa73ef600f267705b6a38d7975b0
Dhananjay <dhananjayman11@gmail.com>
2022-01-08 18:49:53 +0530

    feat: create product redux

---
+++ src/redux/productSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    products: [],
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    getProductStart: (state) => {
+      state.isFetching = true;
+    },
+    getProductSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products = action.payload;
+    },
+    getProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    }
+  },
+});
+
+export const { getProductStart, getProductSuccess, getProductFailure } = productSlice.actions;
+export default productSlice.reducer;
