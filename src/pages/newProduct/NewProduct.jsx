5832f0a2b7c41e646f80ebebcc2cb580e26858b2
Dhananjay <dhananjayman11@gmail.com>
2022-01-11 12:40:54 +0530

    feat: Add product functionality

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,56 @@
+import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [imgFile, setImgFile] = useState();
+  const [category, setCategory] = useState();
+
+  const handleChange = (event) => {
+    setInputs(prev => {
+      return { ...prev, [event.target.name]: event.target.value }
+    })
+  }
+
+  const handleCategoryChange = (event) => {
+    // console.log(event.target.value);
+    setCategory(event.target.value.split(','));
+  }
+  const handleSubmit = (event) => {
+    e.preventDefault();
+  }
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type="file" id="file" onChange={(event) => setImgFile(event.target.files)[0]} />
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Title</label>
+          <input type="text" placeholder="iPhone 13 Pro Max" name="title" onChange={handleChange} />
+        </div>
+        <div className="addProductItem" >
+          <label>Category</label>
+          <input type="text" placeholder="jeans,shirt" onChange={handleCategoryChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Description</label>
+          <input type="text" placeholder="Product Descripiton..." name="desc" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Price</label>
+          <input type="text" placeholder="2000" name="price" onChange={handleChange} />
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <input type="text" placeholder="123" />
-        </div>
-        <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <select name="" id="" name="inStock" onChange={handleChange}>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button className="addProductButton" onClick={handleSubmit}>Create</button>
       </form>
     </div>
   );
